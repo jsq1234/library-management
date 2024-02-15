d1502c5160d038d7083dcf938793591b5d14efd5
jsq1234 <mananprakash3078@gmail.com>
2024-02-15 13:46:58 +0530

    added Post mapping for adding a book

--- backend/api/src/main/java/library/backend/api/controllers/BookController.java
+++ backend/api/src/main/java/library/backend/api/controllers/BookController.java
@@ -1,9 +1,12 @@
 package library.backend.api.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +28,11 @@ public class BookController {
         }
 
         return ResponseEntity.ok(book);
+    }
+
+    @PostMapping()
+    public ResponseEntity<Book> addBook(@RequestBody Book book) {
+        Book savedBook = bookRepository.save(book);
+        return new ResponseEntity<>(savedBook, HttpStatusCode.valueOf(200));
     }
 }
