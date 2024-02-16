d60d399e44e166717ffa473261b928fc02dc1636
jsq1234 <mananprakash3078@gmail.com>
2024-02-17 01:20:34 +0530

    added JwtAuthFilter

--- backend/api/src/main/java/library/backend/api/models/Student.java
+++ backend/api/src/main/java/library/backend/api/models/Student.java
@@ -1,7 +1,5 @@
 package library.backend.api.models;
 
-import library.backend.api.models.Issue;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
