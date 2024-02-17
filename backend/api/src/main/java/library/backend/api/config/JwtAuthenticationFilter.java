6b67997abd0872264142206db04d58eedce43992
jsq1234 <mananprakash3078@gmail.com>
2024-02-18 03:48:31 +0530

    added custom authentication entry point

--- backend/api/src/main/java/library/backend/api/config/JwtAuthenticationFilter.java
+++ backend/api/src/main/java/library/backend/api/config/JwtAuthenticationFilter.java
@@ -14,6 +14,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import library.backend.api.utils.JwtUtils;
 
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
