5228efe33d59cf77bf4c119290f80d9596c9577c
jsq1234 <mananprakash3078@gmail.com>
2024-02-20 22:39:26 +0530

    created AuthenticationConfig which has beans related to authentication

--- backend/api/src/main/java/library/backend/api/config/SecurityConfig.java
+++ backend/api/src/main/java/library/backend/api/config/SecurityConfig.java
@@ -2,51 +2,25 @@ package library.backend.api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.ProviderManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import library.backend.api.services.EmailDetailsServiceImpl;
-import library.backend.api.services.PhoneNoDetailsServiceImpl;
-
-import java.util.List;
+import lombok.RequiredArgsConstructor;
 
 @Configuration
+@EnableWebSecurity
 @EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
         private final AuthenticationEntryPoint authenticationEntryPoint;
         private final JwtAuthenticationFilter jwtAuthenticationFilter;
-        private final EmailDetailsServiceImpl emailDetailsService;
-        private final PhoneNoDetailsServiceImpl phoneNoDetailsService;
-        // private final UserDetailsService userDetailsService;
-
-        // public SecurityConfig(AuthenticationEntryPoint authenticationEntryPoint,
-        // JwtAuthenticationFilter jwtAuthenticationFilter, UserDetailsService
-        // userDetailsService) {
-        // this.authenticationEntryPoint = authenticationEntryPoint;
-        // this.jwtAuthenticationFilter = jwtAuthenticationFilter;
-        // this.userDetailsService = userDetailsService;
-        // }
-
-        public SecurityConfig(AuthenticationEntryPoint authenticationEntryPoint,
-                        JwtAuthenticationFilter jwtAuthenticationFilter, EmailDetailsServiceImpl emailDetailsService,
-                        PhoneNoDetailsServiceImpl phoneNoDetailsService) {
-                this.authenticationEntryPoint = authenticationEntryPoint;
-                this.jwtAuthenticationFilter = jwtAuthenticationFilter;
-                this.emailDetailsService = emailDetailsService;
-                this.phoneNoDetailsService = phoneNoDetailsService;
-        }
 
         @Bean
         public SecurityFilterChain securityFilterChain(HttpSecurity http)
@@ -77,52 +51,5 @@ public class SecurityConfig {
                 http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
                 return http.build();
-        }
-
-        // @Bean
-        // public AuthenticationManager authenticationManager(
-        // AuthenticationConfiguration config) throws Exception {
-        // return config.getAuthenticationManager();
-        // }
-
-        @Bean
-        public AuthenticationManager authenticationManager(PasswordEncoder passwordEncoder) {
-                return new ProviderManager(
-                                List.of(emailAuthenticationProvider(passwordEncoder),
-                                                phoneNoAuthenticationProvider(passwordEncoder)));
-        }
-
-        @Bean
-        public AuthenticationProvider emailAuthenticationProvider(PasswordEncoder passwordEncoder) {
-                var authenticationProvider = new DaoAuthenticationProvider();
-                authenticationProvider.setUserDetailsService(emailDetailsService);
-                authenticationProvider.setPasswordEncoder(passwordEncoder);
-
-                return authenticationProvider;
-        }
-
-        @Bean
-        AuthenticationProvider phoneNoAuthenticationProvider(PasswordEncoder passwordEncoder) {
-                var authenticationProvider = new DaoAuthenticationProvider();
-                authenticationProvider.setUserDetailsService(phoneNoDetailsService);
-                authenticationProvider.setPasswordEncoder(passwordEncoder);
-
-                return authenticationProvider;
-        }
-
-        // @Bean
-        // public AuthenticationProvider authenticationProvider(
-        // UserDetailsService userDetailsService,
-        // PasswordEncoder passwordEncoder) {
-        // var authenticationProvider = new DaoAuthenticationProvider();
-        // authenticationProvider.setUserDetailsService(userDetailsService);
-        // authenticationProvider.setPasswordEncoder(passwordEncoder);
-
-        // return authenticationProvider;
-        // }
-
-        @Bean
-        public PasswordEncoder bCryptPasswordEncoder() {
-                return new BCryptPasswordEncoder();
         }
 }
--- backend/api/src/main/java/library/backend/api/repositories/UserRepository.java
+++ backend/api/src/main/java/library/backend/api/repositories/UserRepository.java
@@ -3,9 +3,11 @@ package library.backend.api.repositories;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import library.backend.api.models.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmailOrPhoneNo(String email, String username);
 
