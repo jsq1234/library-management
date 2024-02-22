29bbd333ab3f1bef6cac34122cabafafe5c9763e
jsq1234 <mananprakash3078@gmail.com>
2024-02-23 04:03:06 +0530

    enabled cors for localhost:4200 (frontend)

--- backend/api/src/main/java/library/backend/api/config/SecurityConfig.java
+++ backend/api/src/main/java/library/backend/api/config/SecurityConfig.java
@@ -10,7 +10,11 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
 import lombok.RequiredArgsConstructor;
 
 @Configuration
@@ -22,12 +26,21 @@ public class SecurityConfig {
         private final AuthenticationEntryPoint authenticationEntryPoint;
         private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
+        CorsConfigurationSource corsConfigurationSource() {
+                CorsConfiguration configuration = new CorsConfiguration();
+                configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+                configuration.setAllowedMethods(Arrays.asList("*"));
+                configuration.setAllowedHeaders(Arrays.asList("*"));
+                UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+                source.registerCorsConfiguration("/**", configuration);
+                return source;
+        }
+
         @Bean
         public SecurityFilterChain securityFilterChain(HttpSecurity http)
                         throws Exception {
-                // disable cors
-                http.cors(AbstractHttpConfigurer::disable);
-
+                // enable cors with the above config
+                http.cors(cors -> cors.configurationSource(corsConfigurationSource()));
                 // disable csrf
                 http.csrf(AbstractHttpConfigurer::disable);
 
