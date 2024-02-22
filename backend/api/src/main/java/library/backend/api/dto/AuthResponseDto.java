e91dbc7a914b92f0c326306383ecaae7afe6c5a2
jsq1234 <mananprakash3078@gmail.com>
2024-02-22 21:12:06 +0530

    Changed authresponse dto so that it sends user info with jwt token

--- backend/api/src/main/java/library/backend/api/dto/AuthResponseDto.java
+++ backend/api/src/main/java/library/backend/api/dto/AuthResponseDto.java
@@ -1,7 +1,18 @@
 package library.backend.api.dto;
 
 import library.backend.api.utils.AuthStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
-public record AuthResponseDto(String token, AuthStatus authStatus) {
-
+@RequiredArgsConstructor
+@Builder
+@Data
+public class AuthResponseDto {
+    private final String email;
+    private final String phoneNo;
+    private final String token;
+    private final String name;
+    private final AuthStatus status;
 }
