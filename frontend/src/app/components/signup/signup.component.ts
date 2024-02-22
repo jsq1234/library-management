f400c466551a706664505ad17aafa42e4904d169
jsq1234 <mananprakash3078@gmail.com>
2024-02-23 05:27:53 +0530

    created html form and added css for signup page

--- frontend/src/app/components/signup/signup.component.ts
+++ frontend/src/app/components/signup/signup.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent {
+  signUpForm = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
+    name: ['', Validators.required],
+    phoneNo: ['', Validators.required, Validators.min(10), Validators.max(10)],
+  });
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private authService: AuthService
+  ) {}
 
+  get email() {
+    return this.signUpForm.controls['email'];
+  }
+
+  get password() {
+    return this.signUpForm.controls['password'];
+  }
+
+  get phoneNo() {
+    return this.signUpForm.controls['phoneNo'];
+  }
+
+  get name() {
+    return this.signUpForm.controls['name'];
+  }
 }
