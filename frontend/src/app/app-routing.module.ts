1a62dfcd4618363a810636b657e62a6dd4994cb4
jsq1234 <mananprakash3078@gmail.com>
2024-02-23 06:55:17 +0530

    added auth and user guard to guard against unauthorized access and remove signup/login pages after authentication

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,19 +3,24 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { HomeComponent } from './components/home/home.component';
+import { userGuard } from './guards/user.guard';
+import { authGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent,
+    canActivate: [userGuard],
   },
   {
     path: 'signup',
     component: SignupComponent,
+    canActivate: [userGuard],
   },
   {
     path: 'home',
     component: HomeComponent,
+    canActivate: [authGuard],
   },
   {
     path: '',
