3aec91c676de915e315338e5bcc6b3b950d9be6f
jsq1234 <mananprakash3078@gmail.com>
2024-02-23 16:24:55 +0530

    changed /login page to a container that allows user to pick whether to login using email or using phoneNo

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,15 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
+import { LoginEmailComponent } from './components/login-email/login-email.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { HomeComponent } from './components/home/home.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { LogoutButtonComponent } from './components/logout-button/logout-button.component';
+import { LoginPhonenoComponent } from './components/login-phoneno/login-phoneno.component';
+import { LoginContainerComponent } from './components/login-container/login-container.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, SignupComponent, HomeComponent, LogoutButtonComponent],
+  declarations: [
+    AppComponent,
+    LoginEmailComponent,
+    SignupComponent,
+    HomeComponent,
+    LogoutButtonComponent,
+    LoginPhonenoComponent,
+    LoginContainerComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
