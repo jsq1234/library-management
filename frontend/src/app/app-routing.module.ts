2ad519a4813ebc5363f7f431133b901d9ad32ce0
jsq1234 <mananprakash3078@gmail.com>
2024-02-24 14:46:45 +0530

    increased side-bar width and re-added auth guard to home

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -34,7 +34,7 @@ const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent,
-    //canActivate: [authGuard],
+    canActivate: [authGuard],
     children: [
       {
         path: 'books',
