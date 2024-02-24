283bde7a927405de09873914cfa5c9eef0f837d5
jsq1234 <mananprakash3078@gmail.com>
2024-02-24 22:13:39 +0530

    added user service file for user creation and fetching of list of issues associated with a user

--- backend/api/src/main/java/library/backend/api/models/User.java
+++ backend/api/src/main/java/library/backend/api/models/User.java
@@ -2,6 +2,7 @@ package library.backend.api.models;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
@@ -50,7 +51,7 @@ public class User implements UserDetails {
     @NotBlank(message = "Role is required")
     private String role;
 
-    @OneToMany(mappedBy = "user")
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
     private List<Issue> issues;
 
     @Override
--- backend/api/src/main/java/library/backend/api/repositories/UserRepository.java
+++ backend/api/src/main/java/library/backend/api/repositories/UserRepository.java
@@ -3,6 +3,7 @@ package library.backend.api.repositories;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import library.backend.api.models.User;
@@ -11,6 +12,7 @@ import library.backend.api.models.User;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmailOrPhoneNo(String email, String username);
 
+    @Query("SELECT u FROM User u WHERE u.email = ?1")
     Optional<User> findByEmail(String email);
 
     Optional<User> findByPhoneNo(String phoneNo);
