c8660b0bb0c13bb0a04fc826dbf246b9bd398f84
techieguy22 <2020ucs0107@iitjammu.ac.in>
2024-02-25 20:28:21 +0530

    changed ignore and added controllers/services files

--- backend/api/src/main/java/library/backend/api/models/Issue.java
+++ backend/api/src/main/java/library/backend/api/models/Issue.java
@@ -1,16 +1,16 @@
 package library.backend.api.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Positive;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.GeneratedValue;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import jakarta.persistence.GenerationType;
 
 import java.time.LocalDate;
 
@@ -24,12 +24,27 @@ public class Issue {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull(message = "Issue date cannot be null")
+    @PastOrPresent(message = "Issue date must be in the past or present")
     private LocalDate issueDate;
+
+    @PastOrPresent(message = "Return date must be in the past or present")
     private LocalDate returnDate;
+
+    @NotNull(message = "Issue period cannot be null")
+    @Positive(message = "issuePeriod cannot be negative")
     private Integer issuePeriod;
 
+    @NotNull(message = "User cannot be null")
     @ManyToOne
-    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false)
     private User user;
+
+    // @NotNull(message = "Book cannot be null")
+    @ManyToOne
+    @JoinColumn(name = "book_id", referencedColumnName = "id", nullable = true)
+    private Book book;
+
 
 }
