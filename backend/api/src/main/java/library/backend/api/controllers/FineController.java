019c65796e82ee210d1cb239e3824dd1111015b8
Shrey <shrma.shray@gmail.com>
2024-03-08 15:42:20 +0530

    fine controller added

--- backend/api/src/main/java/library/backend/api/controllers/FineController.java
+++ backend/api/src/main/java/library/backend/api/controllers/FineController.java
@@ -1,9 +1,14 @@
 package library.backend.api.controllers;
 
+import library.backend.api.models.Book;
 import library.backend.api.models.Fine;
+import library.backend.api.models.Issue;
+import library.backend.api.repositories.IssueRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.time.LocalDate;
@@ -12,11 +17,26 @@ import java.time.format.DateTimeFormatter;
 @RestController
 public class FineController {
 
-    @GetMapping(path = "/fine")
-    public ResponseEntity<Fine> getFine(){
+    @Autowired
+    private IssueRepository issueRepository;
+    @GetMapping(path = "/fine/{issue_id}")
+    public ResponseEntity<Fine> getFine(@PathVariable Long issue_id){
 //        LocalDate d1 = LocalDate.now();
 //        LocalDate d2 = d1.minusDays(2);
 //        return ResponseEntity.ok(new Fine(1L,123L,"Sherlock Holmes", d2, d1, 200));
+
+        Issue issued_book = issueRepository.findById(issue_id).orElse(null);
+        if(issued_book == null) return ResponseEntity.notFound().build();
+
+        Book book = issued_book.getBook();
+        LocalDate issueDate = issued_book.getIssueDate();
+        LocalDate returnDate = issued_book.getReturnDate();
+        Integer issuePeriod = issued_book.getIssuePeriod();
+
+        if(returnDate.minusDays(issuePeriod).compareTo(issueDate)>0) return ResponseEntity.ok(new Fine(book.getId(),book.getTitle(), issueDate, returnDate, 100));
+
+        return ResponseEntity.ok(new Fine(book.getId(),book.getTitle(), issueDate, returnDate, 0));
+
     }
 
 }
--- backend/api/src/main/java/library/backend/api/models/Fine.java
+++ backend/api/src/main/java/library/backend/api/models/Fine.java
@@ -4,6 +4,7 @@ package library.backend.api.models;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.cglib.core.Local;
 
 import java.time.LocalDate;
 
@@ -18,4 +19,12 @@ public class Fine {
     LocalDate issueDate;
     LocalDate returnDate;
     Integer fine;
+
+    public Fine(Long book_id, String title, LocalDate issueDate, LocalDate returnDate, Integer fine){
+        this.fine = fine;
+        this.book_id = book_id;
+        this.issueDate = issueDate;
+        this.returnDate = returnDate;
+        this.title = title;
+    }
 }
